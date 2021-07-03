c7b1e1f147784284596df8f81896f2deead8eb61
eug-vs <eug-vs@keemail.me>
2021-07-03 18:50:10 +0300

    chore: add emoji substitute script

---
+++ compile_emoji_sedstring.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd public/emoji;
+for EMOJI in *;
+do
+  BASENAME=${EMOJI%.*}
+  echo "s|:${BASENAME}:|<img src=\"/public/emoji/${EMOJI}\" height=\"24px\" style=\"margin-bottom: -4px\">|g; "
+done
--- convert.sh
+++ convert.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 echo Writing to ${1%.*}.html
+
+if [ -z "${2}" ]; then
+    SEDSTRING=$(./compile_emoji_sedstring.sh)
+else
+    SEDSTRING=$2
+fi
+
 cat "$1" \
-  | sed 's/.md)/.html)/g' \
-  | pandoc -s --from=gfm --to=html -c /style.css -B header.html -H icon.html -M pagetitle="Eugene's Space" --shift-heading-level-by=1 > ${1%.*}.html
+  | sed "s/.md)/.html)/g; $SEDSTRING" \
+  | pandoc -s --from=gfm+emoji --to=html -c /style.css -B header.html -H icon.html -M pagetitle="Eugene's Space" --shift-heading-level-by=1 > ${1%.*}.html
