3fe6701f6ac5dcf949467f0ac582ec67e52fe288
eug-vs <eug-vs@keemail.me>
2021-07-05 00:18:34 +0300

    feat: improve custom emoji accessibility

--- compile_emoji_sedstring.sh
+++ compile_emoji_sedstring.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+SIZE=24px
+STYLE="margin-bottom: -4px;"
+
 cd public/emoji;
 for EMOJI in *;
 do
   BASENAME=${EMOJI%.*}
-  echo "s|:${BASENAME}:|<img src=\\\"public/emoji/${EMOJI}\\\" height=\\\"24px\\\" style=\\\"margin-bottom: -4px\\\">|g; "
+  echo "s|:${BASENAME}:|<img src=\\\"public/emoji/${EMOJI}\\\" alt=\\\"${BASENAME}-icon\\\" height=\\\"${SIZE}\\\" width=\\\"${SIZE}\\\" style=\\\"${STYLE}\\\">|g; "
 done
