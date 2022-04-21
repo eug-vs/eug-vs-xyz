0b7bc99af62aa53148b7fcede648e5dfb6e2ca22
eug-vs <eugene@eug-vs.xyz>
2022-04-21 14:58:28 +0300

    feat: remove old scripts

--- compile_openring_args.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Compile args string for openring from URLS file
-URLS=urls
-
-sed '/^$/d; s/^/-s /;' $URLS
--- hot-reload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Automatically recompile Markdown sources when they change
-
-find -type f -name '*.md' | entr -s "make localize"
