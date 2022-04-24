6efbc747e3b8b0b7524a7584b84740230c6c58f3
eug-vs <eugene@eug-vs.xyz>
2022-04-24 20:18:54 +0300

    blog: creating gentoo ebuilds is easy as fuck

---
+++ config.mk
@@ -0,0 +1,4 @@
+WEBSITE_TITLE=Eugene's Space
+WEBSITE_URL=https://eug-vs.xyz
+CONVERT=pandoc --from=gfm+emoji --to=html --section-divs --shift-heading-level-by=1
+BLOG_PREVIEW_MARKER=Recent blog posts
